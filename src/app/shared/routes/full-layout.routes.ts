741d29bffea09677055bc242a240b254bc599105
Crewnie Master <crewnie@iMac-de-IngAjVillalon.local>
2017-11-09 16:05:03 -0500

    Actualización Jueves 9, Noviembre 2017

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -11,55 +11,11 @@ export const Full_ROUTES: Routes = [
     loadChildren: './pages/full-pages/full-pages.module#FullPagesModule'
   },
   {
-    path: 'dashboard',
-    loadChildren: './dashboard/dashboard.module#DashboardModule'
-  },
-  {
-    path: 'calendar',
-    loadChildren: './calendar/calendar.module#CalendarsModule'
-  },
-  {
-    path: 'charts',
-    loadChildren: './charts/charts.module#ChartsNg2Module'
-  },
-  {
-    path: 'forms',
-    loadChildren: './forms/forms.module#FormModule'
-  },
-  {
-    path: 'maps',
-    loadChildren: './maps/maps.module#MapsModule'
-  },
-  {
-    path: 'tables',
-    loadChildren: './tables/tables.module#TablesModule'
-  },
-  {
-    path: 'datatables',
-    loadChildren: './data-tables/data-tables.module#DataTablesModule'
-  },
-  {
-    path: 'uikit',
-    loadChildren: './ui-kit/ui-kit.module#UIKitModule'
-  },
-  {
-    path: 'components',
-    loadChildren: './components/ui-components.module#UIComponentsModule'
-  },
-  {
-    path: 'cards',
-    loadChildren: './cards/cards.module#CardsModule'
-  },
-  {
-    path: 'colorpalettes',
+    path: 'demo/colors',
     loadChildren: './color-palette/color-palette.module#ColorPaletteModule'
   },
   {
-    path: 'chat',
-    loadChildren: './chat/chat.module#ChatModule'
-  },
-  {
-    path: 'inbox',
-    loadChildren: './inbox/inbox.module#InboxModule'
+    path: 'demo/cards',
+    loadChildren: './cards/cards.module#CardsModule'
   },
 ];
